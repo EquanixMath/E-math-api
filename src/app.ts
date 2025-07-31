5308288937d72348de0eeba0502a9a4f7d5daef2
THITITHAT <118922213+thitithat-tiankrajang@users.noreply.github.com>
2025-07-31 08:43:14 +0700

    set auth

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import authRouter from './router/auth.js';
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:3000','https://ds-bingo-pmprwdascver-2025.vercel.app/'], 
+    origin: ['http://localhost:3000','https://ds-bingo-pmprwdascver-2025.vercel.app'], 
     credentials: true,            
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'],    
